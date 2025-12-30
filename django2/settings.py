8afb781f39fe96ad74456ea4def32c6201f11b23
Rodrigo Teixeira <rodrigoteixeira165@gmail.com>
2025-12-30 11:52:22 -0300

    Envio_atualizado

--- django2/settings.py
+++ django2/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os.path
 from pathlib import Path
-from subprocess import STARTUPINFO
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
